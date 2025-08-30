898d142bb5ed49b636e9142431162f428f0e33ba
Vji <mrvijaykumar.in@gmail.com>
2025-08-30 16:06:25 +0530

    Describe what you changed

---
+++ components/VideoPlayer.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+// import dynamic from "next/dynamic";
+// import ReactPlayer from "react-player";
+
+const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
+
+interface VideoPlayerProps {
+   url: string;
+}
+
+export const VideoPlayer = ({ url }: VideoPlayerProps) => {
+   return (
+      <div className="relative aspect-video">
+         <ReactPlayer src={url} width="100%" height="100%" controls playing={false} />
+      </div>
+   );
+};
